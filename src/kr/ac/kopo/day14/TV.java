1e92644d226317f778664a8b20e8badf8e5e6031
Oseongmin614 <snders@naver.com>
2025-03-24 21:40:34 +0900

    feat:java study 250324

--- src/kr/ac/kopo/day14/TV.java
+++ src/kr/ac/kopo/day14/TV.java
@@ -14,6 +14,6 @@ public interface TV {
 
     default void copyright(){
         //interface에서 메소드는 못쓰지만 default 쓰면 메소드를 쓸수있다.좋은방법은아님
-        System.out.println("모든기능의 표준은 TV협회에서 정의합니다. ");
+        System.out.println("모든기능의 표준은 TV협회에서 정의합니다.  ");
     }
 }
